c511601cf5377a817c135e2a5743ac308a9f2ee2
hyeong1 <mi75625@gmail.com>
2024-06-10 01:55:12 +0900

    Feat: 작성 날짜 utils 함수

---
+++ src/utils/Format.js
@@ -0,0 +1,8 @@
+export const createDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, "0");
+  const day = today.getDate().toString().padStart(2, "0");
+  const format = `${year}-${month}-${day}`;
+  return format;
+};
