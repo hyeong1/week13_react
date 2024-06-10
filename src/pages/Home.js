2a81b00c2ebf58498641eb3ea8e77e95bde9c8fa
hyeong1 <mi75625@gmail.com>
2024-06-11 00:46:59 +0900

    Design: 문제 추천 컴포넌트 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import List from "../components/List";
 import * as styled from "../styles/style";
 import { Link } from "react-router-dom";
+import Recommend from "../components/Recommend";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <styled.HeaderDiv>Home</styled.HeaderDiv>
       <styled.Container>
         <styled.TitleCenter>알고리즘 복습하기</styled.TitleCenter>
+        <Recommend />
         <styled.AlignRight>
           <Link to={`add`}>
             <styled.StButton>추가</styled.StButton>
