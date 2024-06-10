0b867213961a4ba5f2589bff78932f51cfebc9ec
hyeong1 <mi75625@gmail.com>
2024-06-11 01:41:37 +0900

    Fix: 수정 폼에서 수정 안하고 "돌아가기" 했을 때 prevPage 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,15 @@ import React from "react";
 import { FooterDiv, RowCenter } from "../styles/style";
 import { IoIosArrowBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setPrevPage } from "../redux/modules/postSlice";
 
 const Footer = () => {
   const navigator = useNavigate();
+  const dispatch = useDispatch();
   const link = useSelector((state) => state.posts.prevPage);
   const handleBack = () => {
+    dispatch(setPrevPage("/"));
     navigator(link);
   };
 
